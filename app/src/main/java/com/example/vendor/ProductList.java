40a3852ff3c98fb712b05c68956d8b663958f14f
fahad malik <fahadmalik043043@gmail.com>
2023-05-03 13:47:17 +0500

    Fahad Malik

--- app/src/main/java/com/example/vendor/ProductList.java
+++ app/src/main/java/com/example/vendor/ProductList.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,23 +17,28 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductList extends AppCompatActivity {
+public class ProductList extends AppCompatActivity
+{
 
     FloatingActionButton addProduct;
+    MyAdaptor adapter;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
         ListView listView = findViewById(R.id.listview);
+        addProduct = findViewById(R.id.addProduct);
         Intent i = getIntent();
         ArrayList<Product> list = i.getParcelableArrayListExtra("prdList");
         MyAdaptor adapter = new MyAdaptor(this,list);
         listView.setAdapter(adapter);
-        addProduct = findViewById(R.id.addProduct);
         addProduct.setOnClickListener(view -> {
             startActivity(new Intent(ProductList.this, ProductCRUD.class));
+            finish();
         });
     }
+
 }
 
 
